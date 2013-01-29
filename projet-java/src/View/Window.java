0d599a3149564006be47938247c0e631760d7d0b
billiere <david@pc-dell>
2013-01-29 15:21:58 +0100

    suppr warning

--- projet-java/src/View/Window.java
+++ projet-java/src/View/Window.java
@@ -9,6 +9,11 @@ import javax.swing.JPanel;
 
 public class Window extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public Window() {
 		this.setTitle("Projet Java");
 		this.setSize(540, 540);
