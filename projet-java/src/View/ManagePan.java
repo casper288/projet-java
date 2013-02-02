110cef50c0bd36558a837b6211894ad213dc61fc
billiere <david@pc-dell>
2013-02-02 22:14:09 +0100

    manage pan

--- projet-java/src/View/ManagePan.java
+++ projet-java/src/View/ManagePan.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -53,14 +55,59 @@ public class ManagePan {
 		this.heightMap.setPreferredSize(new Dimension(40, 20));
 		this.widthMap.setPreferredSize(new Dimension(40, 20));
 		this.ppSizeMap.setPreferredSize(new Dimension(30, 20));
+		this.manualButton.setPreferredSize(new Dimension(110, 25));
+		this.speedSimulationTextField.setPreferredSize(new Dimension(40, 20));
+		this.numberCycleSimulationTextField.setPreferredSize(new Dimension(40,
+				20));
+		this.startButton.setPreferredSize(new Dimension(110, 25));
 
 		this.mapSizeLabel.setFont(this.police2);
 		this.speedSimulationLabel.setFont(this.police2);
 		this.numberCycleSimulationLabel.setFont(this.police2);
 
+		this.manualButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent arg0) {
+				ManagePan.this.speedSimulationTextField.setEnabled(false);
+				ManagePan.this.numberCycleSimulationTextField.setEnabled(false);
+			}
+		});
+
+		this.automaticButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				ManagePan.this.speedSimulationTextField.setEnabled(true);
+				ManagePan.this.numberCycleSimulationTextField.setEnabled(true);
+			}
+		});
+
 		JPanel ppPanel = new JPanel();
 		ppPanel.setBackground(Color.WHITE);
 		ppPanel.setPreferredSize(new Dimension(290, 15));
+
+		JPanel ppPanel1 = new JPanel();
+		ppPanel1.setBackground(Color.WHITE);
+		ppPanel1.setPreferredSize(new Dimension(290, 15));
+
+		JPanel ppPanel2 = new JPanel();
+		ppPanel2.setBackground(Color.WHITE);
+		ppPanel2.setPreferredSize(new Dimension(290, 15));
+
+		JPanel ppPanel3 = new JPanel();
+		ppPanel3.setBackground(Color.WHITE);
+		ppPanel3.setPreferredSize(new Dimension(15, 15));
+
+		JPanel ppPanel4 = new JPanel();
+		ppPanel4.setBackground(Color.WHITE);
+		ppPanel4.setPreferredSize(new Dimension(50, 15));
+
+		JPanel ppPanel5 = new JPanel();
+		ppPanel5.setBackground(Color.WHITE);
+		ppPanel5.setPreferredSize(new Dimension(400, 15));
+
+		JPanel ppPanel6 = new JPanel();
+		ppPanel6.setBackground(Color.WHITE);
+		ppPanel6.setPreferredSize(new Dimension(400, 5));
 
 		panCenter.add(ppPanel);
 		panCenter.add(this.mapSizeLabel);
@@ -68,14 +115,22 @@ public class ManagePan {
 		panCenter.add(this.xSize);
 		panCenter.add(this.widthMap);
 		panCenter.add(this.ppSizeMap);
+		panCenter.add(ppPanel1);
 		panCenter.add(this.automaticButton);
+
 		panCenter.add(this.manualButton);
+		panCenter.add(ppPanel2);
+
 		panCenter.add(this.speedSimulationLabel);
 		panCenter.add(this.speedSimulationTextField);
+		panCenter.add(ppPanel3);
 		panCenter.add(this.ppSpeedSimulation);
+		panCenter.add(ppPanel6);
 		panCenter.add(this.numberCycleSimulationLabel);
 		panCenter.add(this.numberCycleSimulationTextField);
 		panCenter.add(this.ppNumberCycleSimulation);
+		panCenter.add(ppPanel4);
+		panCenter.add(ppPanel5);
 		panCenter.add(this.startButton);
 
 		container.add(panCenter, BorderLayout.CENTER);
@@ -83,5 +138,4 @@ public class ManagePan {
 		return container;
 
 	}
-
 }
