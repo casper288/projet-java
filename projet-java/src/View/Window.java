140072e5e6a834aaa12ab2bf53eac88eaf536cd5
Musique <Musique@Alexandre-Mangin>
2013-02-04 08:33:14 +0100

    Correction orthographique des commentaires dans la classe Window.java

--- projet-java/src/View/Window.java
+++ projet-java/src/View/Window.java
@@ -15,215 +15,216 @@ import javax.swing.JPanel;
 import Controller.Map.Map;
 
 /**
- * @since Classe de la fenêtre principale dans laquelle on instancie un menu et
+ * @since Classe de la fenêtre principale dans laquelle on instance le menu et
  *        des panel dans un panel général.
  * @see colorPan()
  * @author david, agathe, alexandre
  */
 public class Window extends JFrame {
 
-	// sauvegarde du mode séléctionné.
-	private String mode = "forest";
+    // sauvegarde du mode sélectionné.
+    private String mode = "forest";
 
-	// menu du programme
-	private JMenuBar menuBar = new JMenuBar();
-	private JMenu fileMenu = new JMenu("Fichier");
-	private JMenuItem newMapButton = new JMenuItem("Nouvelle Carte");
-	private JMenu saveMenu = new JMenu("Sauvegarder");
-	private JMenuItem saveBddButton = new JMenuItem("Base de données");
-	private JMenuItem saveCsvButton = new JMenuItem("CSV");
-	private JMenuItem importButton = new JMenuItem("Importer carte");
-	private JMenuItem exitButton = new JMenuItem("Quitter");
-	private JMenu modeMenu = new JMenu("Mode simulation");
-	private JMenuItem forestModeButton = new JMenuItem("Forêt");
-	private JMenuItem fireModeButton = new JMenuItem("Feu de forêt");
-	private JMenuItem insectModeButton = new JMenuItem("Invasion d'insectes");
+    // menu du programme
+    private final JMenuBar menuBar = new JMenuBar();
+    private final JMenu fileMenu = new JMenu("Fichier");
+    private final JMenuItem newMapButton = new JMenuItem("Nouvelle Carte");
+    private final JMenu saveMenu = new JMenu("Sauvegarder");
+    private final JMenuItem saveBddButton = new JMenuItem("Base de données");
+    private final JMenuItem saveCsvButton = new JMenuItem("CSV");
+    private final JMenuItem importButton = new JMenuItem("Importer carte");
+    private final JMenuItem exitButton = new JMenuItem("Quitter");
+    private final JMenu modeMenu = new JMenu("Mode simulation");
+    private final JMenuItem forestModeButton = new JMenuItem("Forêt");
+    private final JMenuItem fireModeButton = new JMenuItem("Feu de forêt");
+    private final JMenuItem insectModeButton = new JMenuItem(
+	    "Invasion d'insectes");
 
-	private JMenuItem helpMenu = new JMenuItem("Aide");
-	private JMenuItem aboutButton = new JMenuItem("À propos");
+    private final JMenuItem helpMenu = new JMenuItem("Aide");
+    private final JMenuItem aboutButton = new JMenuItem("À propos");
 
-	private JPanel panCenter = new JPanel();
-	private JPanel panWest = new JPanel();
-	private JPanel panEast = new JPanel();
-	private JPanel panSouth = new JPanel();
-	private JPanel panNorth = new JPanel();
+    private final JPanel panCenter = new JPanel();
+    private JPanel panWest = new JPanel();
+    private final JPanel panEast = new JPanel();
+    private final JPanel panSouth = new JPanel();
+    private final JPanel panNorth = new JPanel();
 
-	private Map map = null;
+    private Map map = null;
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public Window() {
-		this.setTitle("Simulateur de vie d'une forêt");
-		this.setSize(1024, 768);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.initComponent(); // initialisation du contenu de la fenêtre
-	}
+    public Window() {
+	this.setTitle("Simulateur de vie d'une forêt");
+	this.setSize(1024, 768);
+	this.setLocationRelativeTo(null);
+	this.setResizable(false);
+	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	this.initComponent(); // initialisation du contenu de la fenêtre
+    }
 
-	private void initComponent() {
-		JPanel container = new JPanel();
-		container.setLayout(new BorderLayout());
+    private void initComponent() {
+	JPanel container = new JPanel();
+	container.setLayout(new BorderLayout());
 
-		// organisation de mon menu
-		this.saveMenu.add(this.saveBddButton);
-		this.saveMenu.add(this.saveCsvButton);
-		this.modeMenu.add(this.forestModeButton);
-		this.modeMenu.add(this.fireModeButton);
-		this.modeMenu.add(this.insectModeButton);
-		this.fileMenu.add(this.newMapButton);
-		this.fileMenu.add(this.saveMenu);
-		this.fileMenu.add(this.importButton);
-		this.fileMenu.add(this.exitButton);
-		this.menuBar.add(this.fileMenu);
-		this.menuBar.add(this.modeMenu);
+	// organisation de mon menu
+	this.saveMenu.add(this.saveBddButton);
+	this.saveMenu.add(this.saveCsvButton);
+	this.modeMenu.add(this.forestModeButton);
+	this.modeMenu.add(this.fireModeButton);
+	this.modeMenu.add(this.insectModeButton);
+	this.fileMenu.add(this.newMapButton);
+	this.fileMenu.add(this.saveMenu);
+	this.fileMenu.add(this.importButton);
+	this.fileMenu.add(this.exitButton);
+	this.menuBar.add(this.fileMenu);
+	this.menuBar.add(this.modeMenu);
 
-		// !!!!!!!!!!!!!!!!!!!!!!!!!
-		// !!!création objet map !!!
-		// !!!!!!!!!!!!!!!!!!!!!!!!!
-		this.map = new Map(Window.this);
+	// !!!!!!!!!!!!!!!!!!!!!!!!!
+	// !!!création objet map !!!
+	// !!!!!!!!!!!!!!!!!!!!!!!!!
+	this.map = new Map(Window.this);
 
-		/*
-		 * initialisation de la map vierge en 100 par 100 et enregistrement dans
-		 * le panelWest
-		 */
-		this.panWest = this.map.initMap(this.panWest);
-		this.panWest.setPreferredSize(new Dimension(700, 600));
-
-		// Ecoute du bouton quitter
-		this.exitButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent arg0) {
-				System.exit(0);
-			}
-		});
-
-		// Ecoute du bouton mode forêt
-		this.forestModeButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent arg0) {
-				Window.this.colorPan(208, 239, 114);
-				Window.this.setMode("forest");
-
-			}
-		});
-
-		// Ecoute du bouton mode feu de forêt
-		this.fireModeButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				Window.this.colorPan(255, 176, 176);
-				Window.this.setMode("fire");
-
-			}
-		});
-
-		// Ecoute du bouton mode insectes
-		this.insectModeButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent arg0) {
-				Window.this.colorPan(255, 220, 98);
-				Window.this.setMode("insect");
-			}
-		});
-
-		// Ecoute du bouton nouvelle map
-		this.newMapButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent arg0) {
-				Map map = new Map(Window.this);
-				Window.this.panWest = map.initMap(Window.this.panWest);
-			}
-		});
-
-		// Ecoute du bouton sauvegarder BDD
-		this.saveBddButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent arg0) {
-				Window.this.map.saveMap();
-			}
-		});
-
-		// Ecoute du bouton Export CSV
-		this.saveCsvButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				Window.this.map.exportMap();
-			}
-		});
-
-		// Ecoute du bouton import BDD
-		this.importButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(final ActionEvent e) {
-				Window.this.map.importMap();
-			}
-		});
-
-		this.panCenter.setLayout(new BorderLayout());
-		this.panCenter.setPreferredSize(new Dimension(0, 0));
-
-		// Création du panel informations
-		JPanel panCenterNorth = new JPanel();
-		panCenterNorth.setBackground(Color.WHITE);
-		InfoPan infoPan = new InfoPan(Window.this);
-		panCenterNorth = infoPan.initInfoPan();
-
-		// Création panel gestion simulation
-		JPanel panCenterCenter = new JPanel();
-		panCenterCenter.setBackground(Color.WHITE);
-		ManagePan managePan = new ManagePan(Window.this);
-		panCenterCenter = managePan.initManagePan();
-
-		// ajout des panels dans le panelCenter
-		this.panCenter.add(panCenterNorth, BorderLayout.NORTH);
-		this.panCenter.add(panCenterCenter, BorderLayout.CENTER);
-
-		// taille des panels
-		this.panNorth.setPreferredSize(new Dimension(10, 10));
-		this.panEast.setPreferredSize(new Dimension(10, 20));
-		this.panSouth.setPreferredSize(new Dimension(10, 10));
-
-		// ajout des panels dans le panel principal
-		container.add(this.panNorth, BorderLayout.NORTH);
-		container.add(this.panWest, BorderLayout.WEST);
-		container.add(this.panCenter, BorderLayout.CENTER);
-		container.add(this.panEast, BorderLayout.EAST);
-		container.add(this.panSouth, BorderLayout.SOUTH);
-
-		this.setContentPane(container);
-		this.setJMenuBar(this.menuBar);
-	}
-
-	/**
-	 * @since méthode perméttant de changer la couleur de la fenêtre du
-	 *        programme
-	 * @param color1
-	 * @param color2
-	 * @param color3
+	/*
+	 * initialisation de la map vierge en 100 par 100 et enregistrement dans
+	 * le panelWest
 	 */
-	public void colorPan(final int color1, final int color2, final int color3) {
-		Window.this.panWest.setBackground(new Color(color1, color2, color3));
-		Window.this.panSouth.setBackground(new Color(color1, color2, color3));
-		Window.this.panEast.setBackground(new Color(color1, color2, color3));
-		Window.this.panNorth.setBackground(new Color(color1, color2, color3));
-		Window.this.panCenter.setBackground(new Color(color1, color2, color3));
+	this.panWest = this.map.initMap(this.panWest);
+	this.panWest.setPreferredSize(new Dimension(700, 600));
 
-	}
+	// Écoute du bouton quitter
+	this.exitButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent arg0) {
+		System.exit(0);
+	    }
+	});
 
-	/**
-	 * @since méthode pour récupérer le mode (forest, fire, insect)
-	 * @return {@link String}
-	 */
-	public String getMode() {
-		return this.mode;
-	}
+	// Écoute du bouton mode forêt
+	this.forestModeButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent arg0) {
+		Window.this.colorPan(208, 239, 114);
+		Window.this.setMode("forest");
 
-	/**
-	 * @since methode pour changer le mode (forest, fire, insect)
-	 * @param mode
-	 */
-	public void setMode(final String mode) {
-		this.mode = mode;
-	}
+	    }
+	});
+
+	// Écoute du bouton mode feu de forêt
+	this.fireModeButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Window.this.colorPan(255, 176, 176);
+		Window.this.setMode("fire");
+
+	    }
+	});
+
+	// Écoute du bouton mode insectes
+	this.insectModeButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent arg0) {
+		Window.this.colorPan(255, 220, 98);
+		Window.this.setMode("insect");
+	    }
+	});
+
+	// Écoute du bouton nouvelle map
+	this.newMapButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent arg0) {
+		Map map = new Map(Window.this);
+		Window.this.panWest = map.initMap(Window.this.panWest);
+	    }
+	});
+
+	// Écoute du bouton sauvegarder BDD
+	this.saveBddButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent arg0) {
+		Window.this.map.saveMap();
+	    }
+	});
+
+	// Écoute du bouton Export CSV
+	this.saveCsvButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Window.this.map.exportMap();
+	    }
+	});
+
+	// Écoute du bouton import BDD
+	this.importButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(final ActionEvent e) {
+		Window.this.map.importMap();
+	    }
+	});
+
+	this.panCenter.setLayout(new BorderLayout());
+	this.panCenter.setPreferredSize(new Dimension(0, 0));
+
+	// Création du panel informations
+	JPanel panCenterNorth = new JPanel();
+	panCenterNorth.setBackground(Color.WHITE);
+	InfoPan infoPan = new InfoPan(Window.this);
+	panCenterNorth = infoPan.initInfoPan();
+
+	// Création panel gestion simulation
+	JPanel panCenterCenter = new JPanel();
+	panCenterCenter.setBackground(Color.WHITE);
+	ManagePan managePan = new ManagePan(Window.this);
+	panCenterCenter = managePan.initManagePan();
+
+	// ajout des panels dans le panelCenter
+	this.panCenter.add(panCenterNorth, BorderLayout.NORTH);
+	this.panCenter.add(panCenterCenter, BorderLayout.CENTER);
+
+	// taille des panels
+	this.panNorth.setPreferredSize(new Dimension(10, 10));
+	this.panEast.setPreferredSize(new Dimension(10, 20));
+	this.panSouth.setPreferredSize(new Dimension(10, 10));
+
+	// ajout des panels dans le panel principal
+	container.add(this.panNorth, BorderLayout.NORTH);
+	container.add(this.panWest, BorderLayout.WEST);
+	container.add(this.panCenter, BorderLayout.CENTER);
+	container.add(this.panEast, BorderLayout.EAST);
+	container.add(this.panSouth, BorderLayout.SOUTH);
+
+	this.setContentPane(container);
+	this.setJMenuBar(this.menuBar);
+    }
+
+    /**
+     * @since méthode permettant de changer la couleur de la fenêtre du
+     *        programme
+     * @param color1
+     * @param color2
+     * @param color3
+     */
+    public void colorPan(final int color1, final int color2, final int color3) {
+	Window.this.panWest.setBackground(new Color(color1, color2, color3));
+	Window.this.panSouth.setBackground(new Color(color1, color2, color3));
+	Window.this.panEast.setBackground(new Color(color1, color2, color3));
+	Window.this.panNorth.setBackground(new Color(color1, color2, color3));
+	Window.this.panCenter.setBackground(new Color(color1, color2, color3));
+
+    }
+
+    /**
+     * @since méthode pour récupérer le mode (forest, fire, insect)
+     * @return {@link String}
+     */
+    public String getMode() {
+	return this.mode;
+    }
+
+    /**
+     * @since méthode pour changer le mode (forest, fire, insect)
+     * @param mode
+     */
+    public void setMode(final String mode) {
+	this.mode = mode;
+    }
 }
